e4a937ebb2731130cf527a86dd6484bf5fc6c960
rathishubham7 <shubhamrathi@icloud.com>
2020-12-07 18:24:52 +0800

    adding files

---
+++ ios/RNTorusDirectSDK2/AppDelegate.h
@@ -0,0 +1,10 @@
+#import <React/RCTBridgeDelegate.h>
+#import <UIKit/UIKit.h>
+
+@class RNTorusSwiftDirectSDK;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+
+@end
---
+++ ios/RNTorusDirectSDK2/RNTorusDirectSdk.m
@@ -0,0 +1,17 @@
+#import "RNTorusDirectSdk.h"
+
+@implementation RNTorusDirectSdk
+
+RCT_EXPORT_MODULE()
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
+RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+{
+    // TODO: Implement
+}
+
+@end
