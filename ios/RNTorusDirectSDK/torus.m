41d125f80cc221c6d6710cbeec30e8367bb1a1b4
rathishubham7 <shubhamrathi@icloud.com>
2020-12-08 14:49:37 +0800

    remaped init module

--- ios/RNTorusDirectSDK/torus.m
+++ ios/RNTorusDirectSDK/torus.m
@@ -1,6 +1,6 @@
 #import "React/RCTBridgeModule.h"
 
-@interface RCT_EXTERN_MODULE(RNTorusSwiftDirectSDK, NSObject)
+@interface RCT_EXTERN_REMAP_MODULE(TorusSdk, RNTorusSwiftDirectSDK, NSObject)
 
 _RCT_EXTERN_REMAP_METHOD(init, initialize: (NSDictionary)params, false)
 
