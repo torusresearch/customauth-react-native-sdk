9723433e1c8b07fd26845337434da55230a571f7
rathishubham7 <shubhamrathi@icloud.com>
2020-12-08 14:47:00 +0800

    updated exports to DirectWebSDK's interface

--- ios/RNTorusDirectSDK/Torus.swift
+++ ios/RNTorusDirectSDK/Torus.swift
@@ -37,40 +37,105 @@ public class RNSubVerifierDetails: NSObject{
     }
 }
 
+
+struct DirectWebSDKArgs: Codable {
+    var baseUrl: String
+    var network: String
+    var proxyContractAddress: String?
+    var enableLogging: Bool
+    var redirectToOpener: Bool?
+    var redirectPathName: String?
+    var apiKey: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case baseUrl = "baseUrl"
+        case network = "network"
+        case proxyContractAddress = "proxyContractAddress"
+        case enableLogging = "enableLogging"
+        case redirectToOpener = "redirectToOpener"
+        case redirectPathName = "redirectPathName"
+        case apiKey = "apiKey"
+    }
+    
+    init(dictionary: [String: Any]) throws {
+        self = try JSONDecoder().decode(DirectWebSDKArgs.self, from: JSONSerialization.data(withJSONObject: dictionary))
+    }
+
+}
+
+struct SubVerifierDetailsWebSDK: Codable {
+    var typeOfLogin: String
+    var verifier: String
+    var clientId: String
+    var jwtParams: [String: String]?;
+    var hash: String?;
+    var queryParameters: [String: String]?;
+    
+    enum CodingKeys: String, CodingKey {
+        case typeOfLogin = "typeOfLogin"
+        case verifier = "verifier"
+        case clientId = "clientId"
+        case jwtParams = "jwtParams"
+        case hash = "hash"
+        case queryParameters = "queryParameters"
+    }
+    
+    init(dictionary: [String: Any]) throws {
+        self = try JSONDecoder().decode(SubVerifierDetailsWebSDK.self, from: JSONSerialization.data(withJSONObject: dictionary))
+    }
+}
+
+
 @objc(RNTorusSwiftDirectSDK)
 public class RNTorusSwiftDirectSDK: NSObject {
     var tdsdk: TorusSwiftDirectSDK?
+    var directAuthVariables: DirectWebSDKArgs?
+    var sub: [SubVerifierDetailsWebSDK] = []
     
-    var sub: [SubVerifierDetails] = []
-    
-    @objc public func addSubverifier(_ loginType: String = "web", loginProvider: String, clientId: String, subverifierId: String, redirectURL: String, extraQueryParams: [String:String] = [:], jwtParams: [String:String] = [:]){
-        var tempLoginType: SubVerifierType = .web
-        let tempLoginProvider: LoginProviders = LoginProviders(rawValue: loginProvider)!
-        
-        if(loginType == "web"){
-            tempLoginType = .web
-        }else if(loginType == "installed"){
-            tempLoginType = .installed
-        }
-        
-        self.sub.append(SubVerifierDetails(loginType: tempLoginType, loginProvider: tempLoginProvider, clientId: clientId, verifierName: subverifierId, redirectURL: redirectURL, extraQueryParams: extraQueryParams, jwtParams: jwtParams))
-        
+    @objc public func initialize(_ params: [String: Any]){
+        self.directAuthVariables = try! JSONDecoder().decode(DirectWebSDKArgs.self, from: JSONSerialization.data(withJSONObject: params))
     }
     
-    @objc public func init2(_ aggregateVerifierType: String, aggregateVerifierName: String, loglevel: Int = 0){
-        let tempAggregateVerifierTypes = verifierTypes(rawValue: aggregateVerifierType)!
-        let tempSubs = self.sub
-        let tempLogLevel = BestLogger.Level(rawValue: loglevel)!
-        
-        self.tdsdk = TorusSwiftDirectSDK(aggregateVerifierType: tempAggregateVerifierTypes, aggregateVerifierName: aggregateVerifierName, subVerifierDetails: tempSubs, loglevel: tempLogLevel)
-    }
+//    @objc public func addSubverifier(_ loginType: String = "web", loginProvider: String, clientId: String, subverifierId: String, redirectURL: String, extraQueryParams: [String:String] = [:], jwtParams: [String:String] = [:]){
+//        var tempLoginType: SubVerifierType = .web
+//        let tempLoginProvider: LoginProviders = LoginProviders(rawValue: loginProvider)!
+//
+//        if(loginType == "web"){
+//            tempLoginType = .web
+//        }else if(loginType == "installed"){
+//            tempLoginType = .installed
+//        }
+//
+//        self.sub.append(SubVerifierDetails(loginType: tempLoginType, loginProvider: tempLoginProvider, clientId: clientId, verifierName: subverifierId, redirectURL: redirectURL, extraQueryParams: extraQueryParams, jwtParams: jwtParams))
+//
+//    }
+//
+//    @objc public func init2(_ aggregateVerifierType: String, aggregateVerifierName: String, loglevel: Int = 0){
+//        let tempAggregateVerifierTypes = verifierTypes(rawValue: aggregateVerifierType)!
+//        let tempSubs = self.sub
+//        let tempLogLevel = BestLogger.Level(rawValue: loglevel)!
+//
+//        self.tdsdk = TorusSwiftDirectSDK(aggregateVerifierType: tempAggregateVerifierTypes, aggregateVerifierName: aggregateVerifierName, subVerifierDetails: tempSubs, loglevel: tempLogLevel)
+//    }
     
-    @objc func triggerLogin(_ callback: @escaping RCTResponseSenderBlock){
+//    @objc func triggerLogin(_ callback: @escaping RCTResponseSenderBlock){
+//        self.tdsdk!.triggerLogin(browserType: .external).done{ data in
+//            print("private key rebuild", data)
+//            callback([data])
+//        }.catch{ err in
+//            print(err)
+//        }
+//    }
+    
+    @objc public func triggerLogin(_ params: [String:Any], resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock){
+        self.sub.append(try! JSONDecoder().decode(SubVerifierDetailsWebSDK.self, from: JSONSerialization.data(withJSONObject: params)))
+        
         self.tdsdk!.triggerLogin(browserType: .external).done{ data in
             print("private key rebuild", data)
-            callback([data])
+            resolve(data)
         }.catch{ err in
             print(err)
+            reject("100", "err", err)
         }
     }
     
--- ios/RNTorusDirectSDK/torus.m
+++ ios/RNTorusDirectSDK/torus.m
@@ -1,38 +1,17 @@
 #import "React/RCTBridgeModule.h"
 
-@interface RCT_EXTERN_MODULE (RNSubVerifierDetails, NSObject)
-
-RCT_EXTERN_METHOD(
-                  init2: (NSString)loginType
-                  loginProvider: (NSString)loginProvider
-                  clientId: (NSString)clientId
-                  subverifierId: (NSString)subverifierId
-                  redirectURL: (NSString)redirectURL
-                  extraQueryParams: (NSDictionary<NSString *, NSString *>)extraQueryParams
-                  jwtParams: (NSDictionary<NSString *, NSString *>)jwtParams
-                  )
-@end
-
 @interface RCT_EXTERN_MODULE(RNTorusSwiftDirectSDK, NSObject)
 
+_RCT_EXTERN_REMAP_METHOD(init, initialize: (NSDictionary)params, false)
+
 RCT_EXTERN_METHOD(
-                  addSubverifier: (NSString)loginType
-                  loginProvider: (NSString)loginProvider
-                  clientId: (NSString)clientId
-                  subverifierId: (NSString)subverifierId
-                  redirectURL: (NSString)redirectURL
-                  extraQueryParams: (NSDictionary)extraQueryParams
-                  jwtParams: (NSDictionary)jwtParams
+                  initialize: (NSDictionary)params
                   )
 
 RCT_EXTERN_METHOD(
-                  init2: (NSString)aggregateVerifierType
-                  aggregateVerifierName: (NSString)aggregateVerifierName
-                  loglevel: (NSInteger)loglevel
-                  )
-
-RCT_EXTERN_METHOD(
-                  triggerLogin: (RCTResponseSenderBlock)callback
+                  triggerLogin: (NSDictionary)params
+                  resolver: (RCTPromiseResolveBlock)resolve
+                  rejecter: (RCTPromiseRejectBlock)reject
                   )
 
 RCT_EXTERN_METHOD(
@@ -41,4 +20,45 @@ RCT_EXTERN_METHOD(
 
 @end
 
+//@interface RCT_EXTERN_MODULE (RNSubVerifierDetails, NSObject)
+//
+//RCT_EXTERN_METHOD(
+//                  init2: (NSString)loginType
+//                  loginProvider: (NSString)loginProvider
+//                  clientId: (NSString)clientId
+//                  subverifierId: (NSString)subverifierId
+//                  redirectURL: (NSString)redirectURL
+//                  extraQueryParams: (NSDictionary<NSString *, NSString *>)extraQueryParams
+//                  jwtParams: (NSDictionary<NSString *, NSString *>)jwtParams
+//                  )
+//@end
+//
+//@interface RCT_EXTERN_MODULE(RNTorusSwiftDirectSDK, NSObject)
+//
+//RCT_EXTERN_METHOD(
+//                  addSubverifier: (NSString)loginType
+//                  loginProvider: (NSString)loginProvider
+//                  clientId: (NSString)clientId
+//                  subverifierId: (NSString)subverifierId
+//                  redirectURL: (NSString)redirectURL
+//                  extraQueryParams: (NSDictionary)extraQueryParams
+//                  jwtParams: (NSDictionary)jwtParams
+//                  )
+//
+//RCT_EXTERN_METHOD(
+//                  init2: (NSString)aggregateVerifierType
+//                  aggregateVerifierName: (NSString)aggregateVerifierName
+//                  loglevel: (NSInteger)loglevel
+//                  )
+//
+//RCT_EXTERN_METHOD(
+//                  triggerLogin: (RCTResponseSenderBlock)callback
+//                  )
+//
+//RCT_EXTERN_METHOD(
+//                  handle: (NSString)url
+//                  )
+//
+//@end
+//
 
