07e917a645d1a85fe33bb2cbfad43d803c866321
rathishubham7 <shubhamrathi@icloud.com>
2020-12-09 13:48:07 +0800

    update RCT EXTERN triggerAggregateLogin

--- ios/Torus.m
+++ ios/Torus.m
@@ -15,50 +15,14 @@ RCT_EXTERN_METHOD(
                   )
 
 RCT_EXTERN_METHOD(
+                  triggerAggregateLogin: (NSDictionary)params
+                  resolver: (RCTPromiseResolveBlock)resolve
+                  rejecter: (RCTPromiseRejectBlock)reject
+                  )
+
+RCT_EXTERN_METHOD(
                   handle: (NSString)url
                   )
 
 @end
-
-//@interface RCT_EXTERN_MODULE (RNSubVerifierDetails, NSObject)
-//
-//RCT_EXTERN_METHOD(
-//                  init2: (NSString)loginType
-//                  loginProvider: (NSString)loginProvider
-//                  clientId: (NSString)clientId
-//                  subverifierId: (NSString)subverifierId
-//                  redirectURL: (NSString)redirectURL
-//                  extraQueryParams: (NSDictionary<NSString *, NSString *>)extraQueryParams
-//                  jwtParams: (NSDictionary<NSString *, NSString *>)jwtParams
-//                  )
-//@end
-//
-//@interface RCT_EXTERN_MODULE(RNTorusSwiftDirectSDK, NSObject)
-//
-//RCT_EXTERN_METHOD(
-//                  addSubverifier: (NSString)loginType
-//                  loginProvider: (NSString)loginProvider
-//                  clientId: (NSString)clientId
-//                  subverifierId: (NSString)subverifierId
-//                  redirectURL: (NSString)redirectURL
-//                  extraQueryParams: (NSDictionary)extraQueryParams
-//                  jwtParams: (NSDictionary)jwtParams
-//                  )
-//
-//RCT_EXTERN_METHOD(
-//                  init2: (NSString)aggregateVerifierType
-//                  aggregateVerifierName: (NSString)aggregateVerifierName
-//                  loglevel: (NSInteger)loglevel
-//                  )
-//
-//RCT_EXTERN_METHOD(
-//                  triggerLogin: (RCTResponseSenderBlock)callback
-//                  )
-//
-//RCT_EXTERN_METHOD(
-//                  handle: (NSString)url
-//                  )
-//
-//@end
-//
 
