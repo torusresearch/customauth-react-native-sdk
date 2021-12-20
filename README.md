25ae40d87880dc9927504898001c01a34542d09e
michaellee8 <ckmichael8@gmail.com>
2021-12-20 14:42:57 +0800

    feat: wip

--- README.md
+++ README.md
@@ -82,4 +82,25 @@ const credentials = await CustomAuth.triggerLogin({
 });
 ```
 
+## FAQ
+
+1. I got `BigInt not found` build error when building for iOS, what should I do?
+
+Add the following snipplet to your `ios/Podfile`. See `example2/ios/Podfile` for a full example.
+
+```ruby
+  post_install do |installer|
+    installer.pods_project.targets.each do |target|
+      if target.name == "web3.swift"
+        target.build_configurations.each do |config|
+          config.build_settings["SWIFT_INCLUDE_PATHS[sdk=iphonesimulator*]"] = "$(inherited) $(PODS_CONFIGURATION_BUILD_DIR)/BigInt $(PODS_CONFIGURATION_BUILD_DIR)/GenericJSON $(PODS_TARGET_SRCROOT)/web3swift/lib/**"
+          config.build_settings["SWIFT_INCLUDE_PATHS[sdk=iphoneos*]"] = "$(inherited) $(PODS_CONFIGURATION_BUILD_DIR)/BigInt $(PODS_CONFIGURATION_BUILD_DIR)/GenericJSON $(PODS_TARGET_SRCROOT)/web3swift/lib/**"
+        end
+      end
+    end
+  end
+```
+
+This is a temporary mitigation for broken xcconfig in the podspec of web3.swift. You may know more at https://github.com/argentlabs/web3.swift/pull/161.
+
 Want to know more or implement more advanced use cases? See our [API reference](https://docs.tor.us/customauth/api-reference/usage).
--- ios/Torus.swift
+++ ios/Torus.swift
@@ -37,18 +37,19 @@ public class RNCustomAuthSdk: NSObject {
                                          extraQueryParams: subverifierWeb.queryParameters ?? [:],
                                          jwtParams: subverifierWeb.jwtParams ?? [:])
             
-            var logvalue: Int = 5
-            if(self.directAuthArgs!.enableLogging != nil && self.directAuthArgs!.enableLogging == true){
-                logvalue = 0
-            }
+
             
             self.tdsdk = CustomAuth(aggregateVerifierType: .singleLogin, aggregateVerifierName: subverifierWeb.verifier, subVerifierDetails: [sub], loglevel: .info)
             
-            self.tdsdk!.triggerLogin(browserType: .external).done{ data in
-                resolve(data)
-            }.catch{ err in
-                reject("400", "triggerLogin: ", err)
+            DispatchQueue.main.async {
+                self.tdsdk!.triggerLogin(browserType: .external).done{ data in
+                    resolve(data)
+                }.catch{ err in
+                    reject("400", "triggerLogin: ", err)
+                }
             }
+            
+            
             
         }catch let err as NSError {
             print("JSON decode failed: \(err.localizedDescription)")
@@ -81,11 +82,15 @@ public class RNCustomAuthSdk: NSObject {
             
             self.tdsdk = CustomAuth(aggregateVerifierType: verifierTypes(rawValue: aggregateVerifierWeb.aggregateVerifierType)!, aggregateVerifierName: aggregateVerifierWeb.verifierIdentifier, subVerifierDetails: [sub], loglevel: .info)
             
-            self.tdsdk!.triggerLogin(browserType: .external).done{ data in
-                resolve(data)
-            }.catch{ err in
-                reject("400", "triggerAggregateLogin: ", err)
+            DispatchQueue.main.async {
+                self.tdsdk!.triggerLogin(browserType: .external).done{ data in
+                    resolve(data)
+                }.catch{ err in
+                    reject("400", "triggerAggregateLogin: ", err)
+                }
             }
+            
+            
         }catch let err as NSError {
             print("JSON decode failed: \(err.localizedDescription)")
             reject("400", "triggerAggregateLogin: ", err.localizedDescription)
@@ -105,12 +110,14 @@ public class RNCustomAuthSdk: NSObject {
       
         self.tdsdk = CustomAuth(aggregateVerifierType: .singleLogin, aggregateVerifierName: verifier, subVerifierDetails: [], loglevel: .info)
         
-        self.tdsdk!.getTorusKey(verifier: verifier, verifierId: verifierId, idToken: idToken, userData: extraParams ?? [:]).done{ data in
-            resolve(data)
-        }.catch{ err in
-            reject("400", "getTorusKey: ", err.localizedDescription)
+        DispatchQueue.main.async {
+            self.tdsdk!.getTorusKey(verifier: verifier, verifierId: verifierId, idToken: idToken, userData: extraParams ?? [:]).done{ data in
+                resolve(data)
+            }.catch{ err in
+                reject("400", "getTorusKey: ", err.localizedDescription)
+            }
         }
-   
+        
     }
     
     @objc public func getAggregateTorusKey(_ verifier: String, verifierId: String, subVerifierInfoArray: Array<[String: String]>, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock){
@@ -126,19 +133,17 @@ public class RNCustomAuthSdk: NSObject {
                 throw "subVerifierInfoArray cannot be empty"
             }
             
-            var logvalue: Int = 5
-            if(self.directAuthArgs!.enableLogging != nil && self.directAuthArgs!.enableLogging == true){
-                logvalue = 0
-            }
             
             self.tdsdk = CustomAuth(aggregateVerifierType: .singleIdVerifier, aggregateVerifierName: verifier, subVerifierDetails: [], loglevel: .info)
             
-            self.tdsdk!.getAggregateTorusKey(verifier: verifier, verifierId: verifierId, idToken: subverifierInfoWebArray[0].idToken, subVerifierDetails: SubVerifierDetails(loginProvider: .jwt, clientId: "", verifierName: verifier, redirectURL: "https://app.tor.us")).done{ data in
-                resolve(data)
-            }.catch{ err in
-                reject("400", "getAggregateTorusKey: ", err)
+            DispatchQueue.main.async {
+                self.tdsdk!.getAggregateTorusKey(verifier: verifier, verifierId: verifierId, idToken: subverifierInfoWebArray[0].idToken, subVerifierDetails: SubVerifierDetails(loginProvider: .jwt, clientId: "", verifierName: verifier, redirectURL: "https://app.tor.us")).done{ data in
+                    resolve(data)
+                }.catch{ err in
+                    reject("400", "getAggregateTorusKey: ", err)
+                }
             }
-            
+
         }catch let err as NSError {
             print("JSON decode failed: \(err.localizedDescription)")
             reject("400", "getAggregateTorusKey: ", err.localizedDescription)
